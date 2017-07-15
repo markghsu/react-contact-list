700d0ab35acaa083f3ce5381ef0c17d014a73117
Mark Hsu <mark.h@canelasoftware.com>
2017-07-15 11:33:35 -0700

    Refactor to use Stateless Functional Component

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,12 +1,11 @@
 import React from 'react'
 
-class ListContacts extends React.Component {
-	render() {
-		return (
-			<ol className='contact-list'>
+function ListContacts(props) {
+	return (
+		<ol className='contact-list'>
 				{
-					this.props.contacts.map(contact => (
-						<li key={contact.id} className='contact-list-item'>{contact.name}
+					props.contacts.map(contact => (
+						<li key={contact.id} className='contact-list-item'>
 							<div className='contact-avatar' style={{
 								backgroundImage: `url(${contact.avatarURL})`
 							}}/>
@@ -22,8 +21,7 @@ class ListContacts extends React.Component {
 					)
 				}
 			</ol>
-		)
-	}
+		);
 }
 
 export default ListContacts
