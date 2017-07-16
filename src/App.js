6be0f4eca0a0438abf24443357045cda0f9a4638
Mark Hsu <mark.h@canelasoftware.com>
2017-07-16 16:38:34 -0700

    Update to use componentDidMount and ContactsAPI

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
 	state = {
-		contacts: [
+		contacts: []
+	}
+
+	componentDidMount() {
+		ContactsAPI.getAll().then((contacts) =>
 			{
-				"id": "ryan",
-				"name": "Ryan Florence",
-				"email": "ryan@reacttraining.com",
-				"avatarURL": "http://localhost:5001/ryan.jpg"
-			},
-			{
-				"id": "michael",
-				"name": "Michael Jackson",
-				"email": "michael@reacttraining.com",
-				"avatarURL": "http://localhost:5001/michael.jpg"
-			},
-			{
-				"id": "tyler",
-				"name": "Tyler McGinnis",
-				"email": "tyler@reacttraining.com",
-				"avatarURL": "http://localhost:5001/tyler.jpg"
-			}
-		]
+				this.setState({contacts});//SAME AS {contacts: contacts}
+			});
 	}
 
 	removeContact = (contact) => {
