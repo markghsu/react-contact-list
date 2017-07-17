4b11456bc33fda9e121753f846e5dd5be9184a40
Mark Hsu <mark.h@canelasoftware.com>
2017-07-16 17:05:29 -0700

    Add CreateContact and switch it in app using screen state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
+import CreateContact from './CreateContact'
 import * as ContactsAPI from './utils/ContactsAPI'
 
 class App extends Component {
 	state = {
+		screen: 'list',
 		contacts: []
 	}
 
@@ -23,7 +25,14 @@ class App extends Component {
 	}
 	render() {
 		return (
-			<ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
+			<div className='app'>
+				{(this.state.screen === 'list' &&
+					<ListContacts onDeleteContact={this.removeContact} contacts={this.state.contacts} />
+				)}
+				{(this.state.screen === 'create' &&
+					<CreateContact />
+				)}
+			</div>
 		)
 	}
 }
